c09793f1d37be7eb2aa1bfc073d8eb555076c824
Sakhiri Mostafa <mostafa.sakhiri@gmail.com>
2020-02-14 10:43:50 +0100

    save routing

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
+import App from './App'
+import Login from './screens/login'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
-serviceWorker.register();
+const routing = (
+  <Router>
+    <div>
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/login'>Login</Link>
+        </li>
+      </ul>
+      <Route exact path='/' component={App} />
+      <Route path='/login' component={Login} />
+    </div>
+  </Router>
+)
+ReactDOM.render(routing, document.getElementById('root'))
