5db1f9aef14403f9fcfe4507c202badcc9ca4c3b
Sakhiri Mostafa <mostafa.sakhiri@gmail.com>
2020-02-28 15:06:21 +0100

    Use videos component in dashboard

--- src/screens/dashboard.js
+++ src/screens/dashboard.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import BottomToolbar from '../components/bottomToolbar/index'
 import TopToolbar from '../components/topToolbar/index'
+import Videos from '../components/videos/index'
 
 function Dashboard() {
   return (
     <div className='dashboard'>
       <BottomToolbar />
+      <Videos />
       <TopToolbar />
     </div>
   )
