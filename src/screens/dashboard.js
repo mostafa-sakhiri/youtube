04143b5a9617f527bf8a9426a3de99718b8d5077
Sakhiri Mostafa <mostafa.sakhiri@gmail.com>
2020-02-28 11:47:37 +0100

    Use bottom toolbar in dashboard

--- src/screens/dashboard.js
+++ src/screens/dashboard.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import BottomToolbar from '../components/bottomToolbar/index'
 
 function Dashboard() {
   return (
     <div className='dashboard'>
-      <h1>Test</h1>
+      <BottomToolbar />
     </div>
   )
 }
