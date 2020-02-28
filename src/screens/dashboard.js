19b5b6dfc4971733250667a5ec95eb32f6bd5a11
Sakhiri Mostafa <mostafa.sakhiri@gmail.com>
2020-02-28 12:36:10 +0100

    Use top toolbar component in dashboard

--- src/screens/dashboard.js
+++ src/screens/dashboard.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import BottomToolbar from '../components/bottomToolbar/index'
+import TopToolbar from '../components/topToolbar/index'
 
 function Dashboard() {
   return (
     <div className='dashboard'>
       <BottomToolbar />
+      <TopToolbar />
     </div>
   )
 }
